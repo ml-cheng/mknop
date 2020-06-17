85d70b7014a48ac88314fd3c78495733d30e5561
ml-cheng <37533217+ml-cheng@users.noreply.github.com>
2020-06-17 20:47:29 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -8,21 +8,6 @@
 # Blog: https://p3terx.com
 #=============================================================
 
-#distfeeds-use-mirrors.bfsu.edu.cn.patch
-diff --git a/include/package-defaults.mk b/include/package-defaults.mk
-index a94f2a4..9567e04 100644
---- a/include/package-defaults.mk
-+++ b/include/package-defaults.mk
-@@ -5,7 +5,7 @@ 
- # See /LICENSE for more information.
- #
- 
--PKG_DEFAULT_DEPENDS = +libc +USE_GLIBC:librt +USE_GLIBC:libpthread
-+PKG_DEFAULT_DEPENDS = +libc +SSP_SUPPORT:libssp +USE_GLIBC:librt +USE_GLIBC:libpthread
- 
- ifneq ($(PKG_NAME),toolchain)
-   PKG_FIXUP_DEPENDS = $(if $(filter kmod-%,$(1)),$(2),$(PKG_DEFAULT_DEPENDS) $(filter-out $(PKG_DEFAULT_DEPENDS),$(2)))
-
 # Add a feed source
 sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
