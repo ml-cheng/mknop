acd2c21e605ab1fe968fcd52c0282644808923fe
ml-cheng <37533217+ml-cheng@users.noreply.github.com>
2020-06-17 20:43:25 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -9,31 +9,19 @@
 #=============================================================
 
 #distfeeds-use-mirrors.bfsu.edu.cn.patch
-diff --git a/package/lean/default-settings/files/zzz-default-settings b/package/lean/default-settings/files/zzz-default-settings
-index a52a824f..7c417e4d 100755
---- a/package/lean/default-settings/files/zzz-default-settings
-+++ b/package/lean/default-settings/files/zzz-default-settings
-@@ -19,6 +19,7 @@ sed -i 's/\"services\"/\"nas\"/g' /usr/lib/lua/luci/controller/aria2.lua
- sed -i 's/services/nas/g' /usr/lib/lua/luci/view/aria2/overview_status.htm
- sed -i 's/\"services\"/\"nas\"/g' /usr/lib/lua/luci/controller/hd_idle.lua
- sed -i 's/\"services\"/\"nas\"/g' /usr/lib/lua/luci/controller/samba.lua
-+sed -i 's/\"services\"/\"nas\"/g' /usr/lib/lua/luci/controller/samba4.lua
- sed -i 's/\"services\"/\"nas\"/g' /usr/lib/lua/luci/controller/minidlna.lua
- sed -i 's/\"services\"/\"nas\"/g' /usr/lib/lua/luci/controller/transmission.lua
- sed -i 's/\"services\"/\"nas\"/g' /usr/lib/lua/luci/controller/mjpg-streamer.lua
-@@ -29,10 +30,12 @@ sed -i 's/services/nas/g'  /usr/lib/lua/luci/view/minidlna_status.htm
+diff --git a/include/package-defaults.mk b/include/package-defaults.mk
+index a94f2a4..9567e04 100644
+--- a/include/package-defaults.mk
++++ b/include/package-defaults.mk
+@@ -5,7 +5,7 @@ 
+ # See /LICENSE for more information.
+ #
  
- ln -sf /sbin/ip /usr/bin/ip
+-PKG_DEFAULT_DEPENDS = +libc +USE_GLIBC:librt +USE_GLIBC:libpthread
++PKG_DEFAULT_DEPENDS = +libc +SSP_SUPPORT:libssp +USE_GLIBC:librt +USE_GLIBC:libpthread
  
--sed -i 's/downloads.openwrt.org/openwrt.proxy.ustclug.org/g' /etc/opkg/distfeeds.conf
-+sed -i 's/downloads.openwrt.org/mirrors.bfsu.edu.cn\/openwrt/g' /etc/opkg/distfeeds.conf
- sed -i 's/http:/https:/g' /etc/opkg/distfeeds.conf
- sed -i 's/root::0:0:99999:7:::/root:$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.:0:0:99999:7:::/g' /etc/shadow
- 
-+sed -i '/Lienol/d' /etc/opkg/distfeeds.conf
-+sed -i '/helloworld/d' /etc/opkg/distfeeds.conf
- sed -i "s/# //g" /etc/opkg/distfeeds.conf
- sed -i '/openwrt_luci/ { s/snapshots/releases\/18.06.8/g; }'  /etc/opkg/distfeeds.conf
+ ifneq ($(PKG_NAME),toolchain)
+   PKG_FIXUP_DEPENDS = $(if $(filter kmod-%,$(1)),$(2),$(PKG_DEFAULT_DEPENDS) $(filter-out $(PKG_DEFAULT_DEPENDS),$(2)))
 
 # Add a feed source
 sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
