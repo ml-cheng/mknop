f59419aa3cb20743bffb76c9d7fb4dc77171a1a3
ml-cheng <37533217+ml-cheng@users.noreply.github.com>
2020-11-13 20:51:38 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -24,4 +24,3 @@ cd lede
 sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 #sed -i '$a src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default
-git clone https://github.com/tuanqing/install-program package/install-program
