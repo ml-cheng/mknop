ebe484540e5fbe6312158a185489ccafa8ebdba1
ml-cheng <37533217+ml-cheng@users.noreply.github.com>
2020-11-16 18:06:03 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -11,34 +11,24 @@ cd lede
 cat >feeds.conf.default <<-EOF
 # src-git luci https://git.openwrt.org/project/luci.git;openwrt-19.07
 # src-git packages https://git.openwrt.org/feed/packages.git
-src-git luci https://github.com/Lienol/openwrt-luci.git;18.06
-src-git packages https://github.com/Lienol/openwrt-packages.git;main
+src-git kenzo https://github.com/kenzok8/openwrt-packages.git
+src-git small https://github.com/kenzok8/small
 src-git routing https://git.openwrt.org/feed/routing.git
 src-git telephony https://git.openwrt.org/feed/telephony.git
-src-git helloworld https://github.com/fw876/helloworld.git
-src-git kenzo https://github.com/kenzok8/openwrt-packages.git
-src-git small https://github.com/kenzok8/small.git
+src-git packages https://github.com/coolsnowwolf/packages
+src-git luci https://github.com/coolsnowwolf/luci
 EOF
-
-rm -rf package/lean/{samba4,luci-app-samba4,luci-app-ttyd}
-svn co https://github.com/Lienol/openwrt/branches/main/package/libs/{libaudit,musl-fts,libsemanage} package/libs
 
 ./scripts/feeds update -a
 ./scripts/feeds install -a
 
-zzz="package/lean/default-settings/files/zzz-default-settings"
-sed -i 's/samba/samba4/' $zzz
-# sed -i 's#openwrt.proxy.ustclug.org#mirrors.bfsu.edu.cn\\/openwrt#' $zzz
-sed -i "/openwrt_luci/i sed -i '/Lienol/d' /etc/opkg/distfeeds.conf" $zzz
-sed -i "/openwrt_luci/i sed -i '/helloworld/d' /etc/opkg/distfeeds.conf" $zzz
-
 packages=" \
-brcmfmac-firmware-43430-sdio brcmfmac-firmware-43455-sdio kmod-brcmfmac wpad \
 kmod-fs-ext4 kmod-fs-vfat kmod-fs-exfat dosfstools e2fsprogs antfs-mount \
 kmod-usb2 kmod-usb3 kmod-usb-storage kmod-usb-storage-extras kmod-usb-storage-uas \
 blkid lsblk parted fdisk cfdisk losetup resize2fs tune2fs pv unzip \
 lscpu htop iperf3 curl lm-sensors install-program 
 "
+
 sed -i '/FEATURES+=/ { s/cpiogz //; s/ext4 //; s/ramdisk //; s/squashfs //; }' \
     target/linux/armvirt/Makefile
 for x in $packages; do
@@ -67,24 +57,15 @@ CONFIG_PACKAGE_luci-app-ttyd=y
 CONFIG_PACKAGE_docker-ce=y
 CONFIG_PACKAGE_luci-app-docker=y
 CONFIG_PACKAGE_luci-app-passwall=y
-CONFIG_PACKAGE_luci-app-passwall_INCLUDE_Trojan=y
+CONFIG_PACKAGE_luci-app-cifs-mount=y
+CONFIG_PACKAGE_luci-app-samba4=y
 ## luci theme
+CONFIG_PACKAGE_luci-theme-bootstrap-mod=y
 CONFIG_PACKAGE_luci-theme-netgear=y
 ## remove
-# CONFIG_PACKAGE_chinadns-ng is not set
-# CONFIG_PACKAGE_kmod-crypto-user is not set
-# CONFIG_PACKAGE_libcares is not set
-# CONFIG_PACKAGE_libhttp-parser is not set
-# CONFIG_PACKAGE_libnghttp2 is not set
-# CONFIG_PACKAGE_luci-app-unblockmusic is not set
 # CONFIG_PACKAGE_luci-app-arpbind is not set
 # CONFIG_PACKAGE_luci-app-autoreboot is not set
-# CONFIG_PACKAGE_luci-app-vsftpd is not set
-# CONFIG_PACKAGE_vsftpd-alt is not set
-# CONFIG_PACKAGE_luci-lib-fs is not set
-# CONFIG_PACKAGE_node is not set
-# CONFIG_PACKAGE_luci-app-passwall_INCLUDE_kcptun is not set										   
-# CONFIG_PACKAGE_luci-app-passwall_INCLUDE_ChinaDNS_NG is not set
+# CONFIG_PACKAGE_luci-app-vsftpd is not set							   
 # CONFIG_DEFAULT_luci-app-adbyby-plus is not set
 # CONFIG_PACKAGE_luci-app-adbyby-plus is not set
 # CONFIG_UnblockNeteaseMusic_Go is not set
@@ -97,7 +78,7 @@ CONFIG_PACKAGE_luci-theme-netgear=y
 # CONFIG_PACKAGE_luci-app-nlbwmon is not set
 # CONFIG_PACKAGE_nlbwmon is not set
 # CONFIG_PACKAGE_luci-app-ramfree is not set
-# CONFIG_PACKAGE_luci-app-ssr-plus is not set# CONFIG_UnblockNeteaseMusic_NodeJS is not set
+# CONFIG_PACKAGE_luci-app-ssr-plus is not set
 # CONFIG_PACKAGE_luci-app-webadmin is not set
 ## others
 CONFIG_BRCMFMAC_SDIO=y
@@ -106,6 +87,5 @@ EOF
 
 make defconfig
 cat .config
-
 
 
